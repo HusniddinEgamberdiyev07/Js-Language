08e65bcc242660bf31e94cf612505f56cf74a4a6
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2025-10-27 19:36:53 +0500

    Object references and copying

--- javascript/objects.js
+++ javascript/objects.js
@@ -97,3 +97,138 @@ console.log("blabla" in user3);     // false
 for(let key in user3){
     console.log(key, user3[key]);
 };
+
+
+
+
+// -- Object references and copying --
+
+// Objects are stored and copied by same refrence.
+// Primitive values are copied as whole value.
+
+// Primitive values. We put copy of nessage into phrase
+
+let message = "hello";
+let phrase = message;
+
+// As a result we have to independent variables, each on storing the string "hello"
+// message 📦 has hello and phrase 📦 has hello too but they are separate boxes which has same value.
+// 📦message -> hello        📦phrase -> hello 
+
+// A variable assigned to an object stores not object itself but its address in memory - in other word a refrence of it 
+
+let user4 = {
+    name:"Husniddin"
+};
+
+// Object has stored somewhere in the memory while variable has a refrence to it.
+// When object is copied, the reference is copied, but object itself is not duplicated.
+
+let admin = user4;
+
+console.log(user4);
+console.log(admin);
+
+// Now we have two variables referencing to the same object.
+// 📦user4 -> object <- 📦admin
+// If one of them changes something it will change object and result will be displayed in both of them.
+// admin changed something in object every variable which has the reference for that object will get that change.
+// 📦user4 <-(change) object (change)-> 📦admin
+
+admin.name = "John";
+console.log(user4);
+console.log(admin);
+
+
+
+
+// Comparsion by reference
+// Two objects are equal if they are the same objects
+
+let a = {};
+let b = a;              // copy reference.
+console.log(a == b);    // true, both variables refrence the same object.
+
+// two independent objects.
+let a2 = {};
+let b2 = {};
+console.log(a2 == b2);  // false, both variables reference different objects.
+
+// const objects can be modified.
+// Because we are not changing the value of variable. We are changing object and reference is the same.
+const user5 = {
+    name:"Husniddin"
+};
+user5.name = "John";    // no error
+
+
+// Cloning and merging
+
+let user6 = {
+    name:"Husniddin",
+    surname:"Egamberdiyev",
+    age:18
+};
+
+let new_user = {};
+
+for(let propKey in user6){
+    new_user[propKey] = user6[propKey]
+}
+
+console.log(new_user);
+
+new_user.name = "John";     // this won't effect user6 cause new_user is independent.
+
+console.log(user6);
+console.log(new_user);
+
+// We can also use the method Object.assign(dest, ...sources).
+// dest is a target object.
+// further arguments are objects which are adding to a target
+
+let new_user2 = {};
+Object.assign(new_user2, user6);
+
+console.log(new_user2);
+
+// if copied property name already exists, it gets overwritten.
+
+let user7 = {name:"Husniddin"};
+console.log(user7);
+
+Object.assign(user7, {name:"John"});
+console.log(user7);
+
+// simpler version of copying.
+
+let user8 = {name:"Husniddin", surname:"Egamberdiyev"};
+let new_user3 = Object.assign({}, user8)
+console.log(new_user3);
+
+
+// Nested cloning
+
+let user9 = {
+    name:"John",
+    sizes:{
+        width:50,
+        height:182
+    }
+};
+
+let new_user4 = Object.assign({}, user9);
+
+console.log(user9.sizes == new_user4.sizes);    // true. It means user9 and new_user4 has sizes which is the same object.
+user9.sizes.width = 45;                         // It will change both user9 and new_user4's width.
+console.log(user9);
+console.log(new_user4);
+
+// To fix this we can use structuredClone(object). It will clone even nested objects.
+let new_user5 = structuredClone(user9);
+console.log(user9.sizes == new_user5.sizes);    // false. They are different.
+new_user5.sizes.width = 30;
+console.log(user9);
+console.log(new_user5);
+
+
